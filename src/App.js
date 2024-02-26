8a40989d82ed5569a66ba747291a11eef0551ee1
fletcher hartsock <fletc@MacBook-Air.local>
2024-02-25 22:48:37 -0500

    Update App.js with personal details

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+          <h1> CS 230L</h1>
+          <h2> Section -002</h2>
+          <p>WVU ID: 800336065</p>
+          <p>Hi I am Fletcher</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
