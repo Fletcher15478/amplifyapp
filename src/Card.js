a824c0863801d1614b1c57fe5d981e230aca69a7
Fletcher15478 <fletchhartsock93@gmail.com>
2024-03-04 00:25:21 -0500

    Third change

--- src/Card.js
+++ src/Card.js
@@ -1,22 +1,30 @@
 import React from 'react';
-<div class="d-flex justify-content-around">
-  <div class="card text-bg-success mb-3" style="max-width: 18rem;">
-    <div class="card-body">
-      <h5 class="card-title">Green Card Title</h5>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+import "bootstrap/dist/css/bootstrap.css";
+
+const Card = () => {
+  return (
+    <div className="d-flex justify-content-around">
+      <div className="card text-bg-success mb-3" style={{ maxWidth: '18rem' }}>
+        <div className="card-body">
+          <h5 className="card-title">Green Card Title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+      <div className="card text-bg-dark mb-3" style={{ maxWidth: '18rem' }}>
+        <div className="card-body">
+          <h5 className="card-title">Black Card Title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+      <div className="card" style={{ maxWidth: '18rem', backgroundColor: 'brown' }}>
+        <div className="card-body text-white">
+          <h5 className="card-title">Brown Card Title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
     </div>
-  </div>
-  <div class="card text-bg-dark mb-3" style="max-width: 18rem;">
-    <div class="card-body">
-      <h5 class="card-title">Black Card Title</h5>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    </div>
-  </div>
-  <div class="card" style="max-width: 18rem; background-color: brown;">
-    <div class="card-body text-white">
-      <h5 class="card-title">Brown Card Title</h5>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    </div>
-  </div>
-</div>
+  );
+}
+
 export default Card;
+
