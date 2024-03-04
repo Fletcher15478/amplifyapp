58a6eb361f45ade86ffc7ba04eb40c52e68c7ae5
Fletcher15478 <fletchhartsock93@gmail.com>
2024-03-03 21:52:46 -0500

    AWS 2

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import logo from './logo.svg';
+import Navbar from './Navbar';
+import Card from './Card'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+          <Navbar />
+          <header className="App-header">
           <h1> CS 230L</h1>
           <h2> Section -002</h2>
           <p>WVU ID: 800336065</p>
           <p>Hi I am Fletcher</p>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          </header>
+          <Card />
     </div>
   );
 }
